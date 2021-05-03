52ac2deab4c8f929587710d5fb5dfaf377780534
Peyman <usestate.pk@gmail.com>
2021-05-04 01:29:32 +0430

    added photo fetching functionality

---
+++ src/app/photos.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID pE6OkypPJ3Rn3HmCqtHJDn02rWZ7uNRYoTMxDc3HtAY'
+      }
+    });
+  }
+}
